8932e3212509d9f1c33bb59967bcaf144ad26c87
Heitor Saqueto <heitorsaqueto@hotmail.com>
2023-06-20 18:07:44 -0300

    final_changes(20/06)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from question_model import Question
 from quiz_brain import QuizBrain
 from data import question_data
+
 question_bank = []
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-# mergido++
 
 quiz = QuizBrain(question_bank)
 
--- question_model.py
+++ question_model.py
@@ -2,6 +2,3 @@ class Question:
     def __init__(self, text_param, answer_param):
         self.text = text_param
         self.answer = answer_param
-
-
-
